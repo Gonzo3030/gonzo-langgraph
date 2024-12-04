07133a5144ac408d7564d1ea7ea312df4693851a
Gonzo3030 <41178245+Gonzo3030@users.noreply.github.com>
2024-12-04 01:05:45 -0600

    Update memory __init__.py

--- gonzo/memory/__init__.py
+++ gonzo/memory/__init__.py
@@ -1,3 +1,5 @@
 from .vector_store import VectorStoreMemory
 from .window import WindowMemory
 from .timeline import TimelineMemory
+
+__all__ = ['VectorStoreMemory', 'WindowMemory', 'TimelineMemory']
