861313543d3529c55d0e6bbb54af6cb2d1c98591
Gonzo3030 <41178245+Gonzo3030@users.noreply.github.com>
2024-12-06 10:21:31 -0600

    Add knowledge graph core components

---
+++ gonzo/graph/knowledge/__init__.py
@@ -0,0 +1,5 @@
+from .graph import KnowledgeGraph
+from .types import Entity, Relationship, Property, TimeAwareEntity
+from .store import GraphStore
+
+__all__ = ['KnowledgeGraph', 'Entity', 'Relationship', 'Property', 'TimeAwareEntity', 'GraphStore']
