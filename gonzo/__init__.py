27b348a32eb3edd9091105571baaee412699590b
Gonzo3030 <41178245+Gonzo3030@users.noreply.github.com>
2024-12-12 16:34:11 -0600

    Add proper package initialization

--- gonzo/__init__.py
+++ gonzo/__init__.py
@@ -1,3 +1,14 @@
-from .agent import GonzoAgent
+"""Gonzo LangGraph Agent - the digital incarnation of Dr. Gonzo from 1974-3030"""
 
 __version__ = "0.1.0"
+
+from .agent import GonzoAgent
+from .types import GonzoState, NextStep
+from .graph import create_workflow
+
+__all__ = [
+    'GonzoAgent',
+    'GonzoState',
+    'NextStep',
+    'create_workflow'
+]
