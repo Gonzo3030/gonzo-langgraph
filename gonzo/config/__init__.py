01e7c7c1f52a4f85bd1a0017cd93b2c4fb94818a
Gonzo3030 <41178245+Gonzo3030@users.noreply.github.com>
2024-12-12 16:34:04 -0600

    Fix config imports structure

--- gonzo/config/__init__.py
+++ gonzo/config/__init__.py
@@ -1,9 +1,11 @@
-from .models import ANTHROPIC_MODEL, GPT4_MODEL
+from .models import MODEL_NAME, ANTHROPIC_MODEL, GPT4_MODEL, MODEL_CONFIGS
 from .analysis import ANALYSIS_CONFIG
 from .constants import *
 
 __all__ = [
+    'MODEL_NAME',
     'ANTHROPIC_MODEL',
     'GPT4_MODEL',
+    'MODEL_CONFIGS',
     'ANALYSIS_CONFIG'
 ]
