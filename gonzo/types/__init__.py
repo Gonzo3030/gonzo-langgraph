139dd39e622b3bf463427317bd1fb6c29d74d9b5
Gonzo3030 <41178245+Gonzo3030@users.noreply.github.com>
2024-12-11 21:25:16 -0600

    Fix types imports by removing non-existent Message type

--- gonzo/types/__init__.py
+++ gonzo/types/__init__.py
@@ -1,10 +1,9 @@
-from .base import GonzoState, NextStep, Message, create_initial_state
+from .base import GonzoState, NextStep, create_initial_state
 from .social import Post, PostMetrics, QueuedPost, PostHistory, InteractionQueue
 
 __all__ = [
     'GonzoState',
     'NextStep',
-    'Message',
     'create_initial_state',
     'Post',
     'PostMetrics',
