8375fe545b08c456d6f6ba299e35dd97a1b0f0ae
Gonzo3030 <41178245+Gonzo3030@users.noreply.github.com>
2024-12-13 15:55:34 -0600

    test(x): Update mock to match tweepy API

--- tests/mocks/x_api.py
+++ tests/mocks/x_api.py
@@ -2,42 +2,51 @@
 
 from datetime import datetime, timezone
 from unittest.mock import MagicMock
-from pytwitter.models import Response, Tweet, User
 
-class MockXApi:
-    """Mock X API client for testing."""
+class MockTweet:
+    """Mock tweet data."""
+    def __init__(self):
+        self.id = '123456789'
+        self.text = 'Test tweet about manipulation patterns'
+        self.author_id = '987654321'
+        self.conversation_id = '123456789'
+        self.created_at = datetime.now(timezone.utc)
+        self.referenced_tweets = None
+        self.context_annotations = None
+
+class MockUser:
+    """Mock user data."""
+    def __init__(self):
+        self.id = '123456789'
+        self.name = 'Gonzo'
+        self.username = 'DrGonzo3030'
+
+class MockResponse:
+    """Mock API response."""
+    def __init__(self, data=None):
+        self.data = data
+
+class MockClient:
+    """Mock tweepy Client for testing."""
     
     def __init__(self, consumer_key=None, consumer_secret=None, access_token=None,
                  access_token_secret=None):
-        self.mock_user = User(
-            id='123456789',
-            name='Gonzo',
-            username='DrGonzo3030'
-        )
-        
-        self.mock_tweet = Tweet(
-            id='123456789',
-            text='Test tweet about manipulation patterns',
-            author_id='987654321',
-            conversation_id='123456789',
-            created_at=datetime.now(timezone.utc).isoformat() + 'Z',
-            referenced_tweets=None,
-            context_annotations=None
-        )
+        self.mock_tweet = MockTweet()
+        self.mock_user = MockUser()
     
-    def create_tweet(self, *args, **kwargs):
-        return Response(data=self.mock_tweet)
+    def create_tweet(self, text, **kwargs):
+        return MockResponse(self.mock_tweet)
     
-    def get_me(self, *args, **kwargs):
-        return Response(data=self.mock_user)
+    def get_me(self, **kwargs):
+        return MockResponse(self.mock_user)
     
-    def get_mentions(self, *args, **kwargs):
-        return Response(data=[self.mock_tweet])
+    def get_users_mentions(self, id, **kwargs):
+        return MockResponse([self.mock_tweet])
     
-    def get_tweets_search_recent(self, *args, **kwargs):
-        return Response(data=[self.mock_tweet])
+    def search_recent_tweets(self, query, **kwargs):
+        return MockResponse([self.mock_tweet])
     
-    def get_rate_limit_status(self, *args, **kwargs):
+    def get_rate_limit_status(self, **kwargs):
         return {
             'resources': {
                 'tweets': {
