ba01d95ee0fa6db6e19720a0b10d076873665b81
Gonzo3030 <41178245+Gonzo3030@users.noreply.github.com>
2024-12-13 15:41:58 -0600

    test(x): Add mock X API for testing

---
+++ tests/mocks/x_api.py
@@ -0,0 +1,49 @@
+"""Mock X API for testing."""
+
+from datetime import datetime, timezone
+from unittest.mock import MagicMock
+from pytwitter.models import Response, Tweet, User
+
+class MockXApi:
+    """Mock X API client for testing."""
+    
+    def __init__(self, *args, **kwargs):
+        self.mock_user = User(
+            id='123456789',
+            name='Gonzo',
+            username='DrGonzo3030'
+        )
+        
+        self.mock_tweet = Tweet(
+            id='123456789',
+            text='Test tweet about manipulation patterns',
+            author_id='987654321',
+            conversation_id='123456789',
+            created_at=datetime.now(timezone.utc).isoformat() + 'Z',
+            referenced_tweets=None,
+            context_annotations=None
+        )
+    
+    def create_tweet(self, *args, **kwargs):
+        return Response(data=self.mock_tweet)
+    
+    def get_me(self, *args, **kwargs):
+        return Response(data=self.mock_user)
+    
+    def get_mentions(self, *args, **kwargs):
+        return Response(data=[self.mock_tweet])
+    
+    def get_tweets_search_recent(self, *args, **kwargs):
+        return Response(data=[self.mock_tweet])
+    
+    def get_rate_limit_status(self, *args, **kwargs):
+        return {
+            'resources': {
+                'tweets': {
+                    '/tweets': {
+                        'limit': 100,
+                        'remaining': 50
+                    }
+                }
+            }
+        }
