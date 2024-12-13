ececeda009fc3cc1a373a5fd53cd16ee06d4058c
Gonzo3030 <41178245+Gonzo3030@users.noreply.github.com>
2024-12-12 19:06:01 -0600

    Update config exports to include task configurations

--- gonzo/config/__init__.py
+++ gonzo/config/__init__.py
@@ -1,8 +1,9 @@
-"""Configuration management for Gonzo LangGraph system."""
+"""Configuration management for Gonzo system."""
 
 from .models import MODEL_NAME, MODEL_CONFIG, GRAPH_CONFIG
 from .analysis import ANALYSIS_CONFIG
 from .prompts import SYSTEM_PROMPT, ANALYSIS_PROMPT_TEMPLATE, RESPONSE_PROMPT_TEMPLATE
+from .tasks import TASK_PROMPTS, TASK_CONFIG
 from .constants import *
 
 __all__ = [
@@ -12,5 +13,7 @@ __all__ = [
     'ANALYSIS_CONFIG',
     'SYSTEM_PROMPT',
     'ANALYSIS_PROMPT_TEMPLATE',
-    'RESPONSE_PROMPT_TEMPLATE'
+    'RESPONSE_PROMPT_TEMPLATE',
+    'TASK_PROMPTS',
+    'TASK_CONFIG'
 ]
