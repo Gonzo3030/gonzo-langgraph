3bc2da743032e2cbcd5a5e9a43240ecaabbb6caa
Gonzo3030 <41178245+Gonzo3030@users.noreply.github.com>
2024-12-17 19:06:34 -0600

    Add main package init file
    - Add version info
    - Set up main imports
    - Define package exports

--- gonzo/__init__.py
+++ gonzo/__init__.py
@@ -1,3 +1,16 @@
-"""Gonzo LangGraph System"""
+"""Gonzo LangGraph project root package."""
 
+# Version
 __version__ = '0.1.0'
+
+# Import main components
+from .state_management import UnifiedState, create_initial_state
+from .monitoring.market_monitor import CryptoMarketMonitor
+from .monitoring.social_monitor import SocialMediaMonitor
+
+__all__ = [
+    'UnifiedState',
+    'create_initial_state',
+    'CryptoMarketMonitor',
+    'SocialMediaMonitor'
+]
