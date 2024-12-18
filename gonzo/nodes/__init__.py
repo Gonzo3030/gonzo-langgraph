2c5c0e14772305c5cc26447dbb5ede5d1d92632a
Gonzo3030 <41178245+Gonzo3030@users.noreply.github.com>
2024-12-17 19:06:46 -0600

    Add nodes package init file
    - Set up narrative generation exports
    - Define package structure

--- gonzo/nodes/__init__.py
+++ gonzo/nodes/__init__.py
@@ -1,9 +1,4 @@
-from .narrative_generation import generate_dynamic_narrative
-from .assessment import assess_content
-from .pattern_detection import detect_patterns
+"""Gonzo processing nodes package."""
+from .narrative_generation import generate_dynamic_narrative, NarrativeOutput
 
-__all__ = [
-    'generate_dynamic_narrative',
-    'assess_content',
-    'detect_patterns'
-]
+__all__ = ['generate_dynamic_narrative', 'NarrativeOutput']
