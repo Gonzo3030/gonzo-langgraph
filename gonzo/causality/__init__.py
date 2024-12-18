157a8c088ab4449d9d2fbb5db4f1ad441a37158f
Gonzo3030 <41178245+Gonzo3030@users.noreply.github.com>
2024-12-17 19:06:58 -0600

    Add causality package init file
    - Set up analyzer exports
    - Add type exports
    - Define package structure

---
+++ gonzo/causality/__init__.py
@@ -0,0 +1,5 @@
+"""Gonzo causality analysis package."""
+from .analyzer import CausalAnalyzer
+from .types import EventCategory, EventScope
+
+__all__ = ['CausalAnalyzer', 'EventCategory', 'EventScope']
